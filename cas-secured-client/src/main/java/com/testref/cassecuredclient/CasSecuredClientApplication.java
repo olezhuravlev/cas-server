c49654748279defb9c5f15bf6829a3b83e6970a7
Oleg Zhuravlev <olezhuravlev@gmail.com>
2021-01-02 13:40:21 +0100

    Init.

---
+++ cas-secured-client/src/main/java/com/testref/cassecuredclient/CasSecuredClientApplication.java
@@ -0,0 +1,13 @@
+package com.testref.cassecuredclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CasSecuredClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CasSecuredClientApplication.class, args);
+	}
+
+}
---
+++ cas-secured-client/src/test/java/com/testref/cassecuredclient/CasSecuredClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.testref.cassecuredclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CasSecuredClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
